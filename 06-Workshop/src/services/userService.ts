536b80efa05cfc9767f8badea9b448feb800e537
dimitar-grigorov <mitak.backtrack@gmail.com>
2023-11-18 07:26:13 +0200

    Added 06-Workshop solution

---
+++ 06-Workshop/src/services/userService.ts
@@ -0,0 +1,18 @@
+import { User } from "../types/userTypes";
+import { HttpService } from "./httpService";
+
+export class UsersService extends HttpService<User> {
+  constructor(baseUrl: string) {
+    super(`${baseUrl}/users`);
+  }
+
+  userGetAll() {
+    // validation
+    this.getAll();
+  }
+
+  getSingleUser(id: number, cb: Function) {
+    this.getOne(id);
+    cb();
+  }
+}
